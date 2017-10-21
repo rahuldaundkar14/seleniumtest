f39b449101218489d1f513b906d9ca310fda386e
Rahul <Rahul@DESKTOP-C46IO30>
2017-10-21 20:47:08 +0530

    push

--- MyTestProject1/src/com/mypackage/TestClass.java
+++ MyTestProject1/src/com/mypackage/TestClass.java
@@ -4,7 +4,7 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Test Project 12356789");
+		System.out.println("Hello Test Project 12356789223 123 ");
 	}
 
 }
