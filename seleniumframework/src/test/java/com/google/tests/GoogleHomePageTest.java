22ba19d881745e7ff11bc231fa2ddb0d630b3460
Rahul <Rahul@DESKTOP-C46IO30>
2017-10-21 16:16:04 +0530

    Adding one new output

--- seleniumframework/src/test/java/com/google/tests/GoogleHomePageTest.java
+++ seleniumframework/src/test/java/com/google/tests/GoogleHomePageTest.java
@@ -7,8 +7,9 @@ public class GoogleHomePageTest {
 	@Test
 	public void mySimpleTest() {
 
-System.out.println("First selenium test...1234567890");
-System.out.println("helloooo");
+		System.out.println("First selenium test...1234567890");
+		System.out.println("helloooo");
+		System.out.println("12345");
 
 	}
 }
