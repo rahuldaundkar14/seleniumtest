b3845e8ef29e84fb6d30d41f27915d95ec128760
Rahul <Rahul@DESKTOP-C46IO30>
2017-10-21 16:38:34 +0530

    added

--- seleniumframework/src/test/java/com/google/tests/GoogleHomePageTest.java
+++ seleniumframework/src/test/java/com/google/tests/GoogleHomePageTest.java
@@ -9,7 +9,7 @@ public class GoogleHomePageTest {
 
 		System.out.println("First selenium test...1234567890123");
 		System.out.println("helloooo123");
-		System.out.println("12345");
+		System.out.println("1234567");
 
 	}
 }
