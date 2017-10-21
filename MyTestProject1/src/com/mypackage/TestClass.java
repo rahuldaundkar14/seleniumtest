70302240d340b2d38db321e5f5ddc91d5b2f6a75
rahuldaundkar14 <32956998+rahuldaundkar14@users.noreply.github.com>
2017-10-21 21:14:27 +0530

    Update TestClass.java

--- MyTestProject1/src/com/mypackage/TestClass.java
+++ MyTestProject1/src/com/mypackage/TestClass.java
@@ -5,6 +5,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Test Project 12356789223 123 12345");
+		System.out.println("done");
 	}
 
 }
