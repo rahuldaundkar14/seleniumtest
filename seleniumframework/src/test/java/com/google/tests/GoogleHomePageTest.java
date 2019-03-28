fb60a22c9a350eb016e620f1c524bcfd50957d88
Rahul Daundkar <rahulda@cybage.com>
2019-03-28 12:23:13 +0530

    committing changes

--- seleniumframework/src/test/java/com/google/tests/GoogleHomePageTest.java
+++ seleniumframework/src/test/java/com/google/tests/GoogleHomePageTest.java
@@ -7,7 +7,7 @@ public class GoogleHomePageTest {
 	@Test
 	public void mySimpleTest() {
 
-		System.out.println("First selenium test...1234567890123");
+		System.out.println("First selenium test...123456789012345666");
 		System.out.println("helloooo123");
 		System.out.println("1234567...okay");
 
